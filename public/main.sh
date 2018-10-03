8e860eace38ca8eed031055a7ce74fec07f28329
dzove855 <v.dzogovic@flash-global.net>
2018-10-03 14:41:20 +0200

    Update main.sh

--- public/main.sh
+++ public/main.sh
@@ -1,32 +1,8 @@
-#!/bin/bash
+# We use the defaul main.sh from yosh
 
-# main file
-
-# use autoloader
-source /usr/share/yosh/autoloader.sh
-
-# get GET and POST and COOKIE variable
-http::read::get
-http::read::post
-http::read::cookie
-
-# redirect stdout and stderr of function to file, to print after
-tmpStdout="$(mktemp -p $TMPDIR)"
-tmpStderr="$(mktemp -p $TMPDIR)"
-
-# Clean TMP file on exit
-trap "rm $tmpStdout; rm $tmpStderr" EXIT
-
-# Save stdout and stderr to a file, to print out the both
-# route::check 1>$tmpStdout 2>$tmpStderr
-$router 1>$tmpStdout 2>$tmpStderr
-
-# Send header
-http::send::out
-
-# send data from route
-[[ -s "$tmpStdout" ]] && cat $tmpStdout
-[[ -s "$tmpStderr" ]] && @error "$(cat $tmpStderr)"
-
-# exit like a pro
-exit
+if [[ -f "/usr/share/yosh/main.sh" ]] 
+then
+    source /usr/share/yosh/main.sh
+else
+    echo "Yosh is missing!"
+fi
